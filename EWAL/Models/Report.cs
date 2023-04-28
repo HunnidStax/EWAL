50b7a38312c03dfbc289c2d78b4c66615a18b35e
HunnidStax <den4ik.trofimenko@gmail.com>
2023-04-28 23:41:59 +0300

    Added base models

---
+++ EWAL/Models/Report.cs
@@ -0,0 +1,33 @@
+﻿namespace EWAL.Models
+{
+    public class Report
+    {
+        private long _id;
+        private string _name;
+
+        public long Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        internal Report(long id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+
+        internal Report() { }
+    }
+
+    public class ReportFactory
+    {
+        public Report Create(long id, string name) => new Report(id, name);
+    }
+}
