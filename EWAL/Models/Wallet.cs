5719ecee69bd3f79f1ca3332678c97b06f581147
HunnidStax <den4ik.trofimenko@gmail.com>
2023-05-11 22:56:06 +0300

    added base services

---
+++ EWAL/Models/Wallet.cs
@@ -0,0 +1,10 @@
+﻿namespace WalletAgent.Models
+{
+    public class Wallet
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public long User { get; set; }
+    }
+}
